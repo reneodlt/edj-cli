41e499355661b34afcba0d10782b134c70177228
Rich Green <rich@reneodlt.com>
2022-10-29 10:46:38 +0100

    this one seems to work for pulling library
    Uses a proper logging config, uses apache for configuration, uses correct maven repo thing (must remove password soon)
    
    Uses a proper logging config, uses apache for configuration, uses correct maven repo thing (must remove password soon)
    
    Uses a proper logging config, uses apache for configuration, uses correct maven repo thing (must remove password soon)
    
    Publishing to a different repo ?
    
    Publishing to a different repo ?
    
    Publishing to a different repo ?
    
    Publishing to a different repo ?
    
    Publishing to a different repo ?
    
    Has the beginnings of playlist management

---
+++ build.gradle.kts
@@ -0,0 +1,104 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.7.10"
+    id("io.ktor.plugin") version "2.1.3"
+    application
+    `maven-publish`
+}
+
+fun versionString() = nf("TAG_NAME")
+
+fun nf(env: String): String {
+    val r = System.getenv(env) ?: ""
+    if (r.length > 0)
+        return r
+    else
+        return "SNAPSHOT"
+}
+
+group = "com.reneodlt"
+version = versionString()
+
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven("https://maven.pkg.github.com/reneodlt/edj-lib") {
+        credentials {
+            username = System.getenv("GITHUB_ACTOR")
+            password = System.getenv("GITHUB_TOKEN")
+        }
+    }
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-configuration2:2.8.0")
+    implementation("commons-beanutils:commons-beanutils:1.9.4")
+
+    implementation("com.reneodlt:edj:v0.18.0")
+//    implementation("com.reneodlt:edj:SNAPSHOT")
+
+    implementation("org.apache.logging.log4j:log4j:2.19.0")
+    implementation("org.slf4j:slf4j-simple:2.0.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+
+    implementation("org.xerial:sqlite-jdbc:3.39.3.0")
+
+    testImplementation(kotlin("test"))
+}
+
+application {
+    mainClass.set("MainKt")
+}
+
+//ktor {
+//    fatJar {
+//        archiveFileName.set("edj-cli.jar")
+//    }
+//}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile>() {
+    kotlinOptions.jvmTarget = "15"
+}
+
+
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom {
+
+            }
+            groupId = "com.reneodlt"
+            artifactId = "edj-cli"
+            version = versionString()
+            from(components["kotlin"])
+            artifact(tasks["shadowJar"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/reneodlt/edj-cli")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+//        maven {
+//            name = "GitHubPackages"
+//            url = uri("https://maven.pkg.github.com/reneodlt/edj-public")
+//            credentials {
+//                username = System.getenv("GITHUB_ACTOR")
+//                password = System.getenv("GITHUB_TOKEN")
+//            }
+//        }
+
+    }
+}
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,82 @@
+import com.reneodlt.edj.EDJ
+import com.reneodlt.edj.EDJPlaylist
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import org.apache.commons.configuration2.builder.fluent.Configurations
+import java.io.File
+import java.nio.file.Path
+import java.util.*
+
+
+fun main(args: Array<String>) {
+    println("Starting...")
+
+    val parser = ArgParser("edj-cli")
+
+    val update by parser.option(ArgType.Boolean, shortName = "u", description = "Perform updates on database")
+
+    parser.parse(args)
+
+    val edj = EDJ()
+    val configs = Configurations()
+
+    val config = configs.properties(File("config.properties"))
+
+    config.keys.forEach {
+        println("File exists flag: ${it} -> ${config.getString(it)}")
+    }
+
+    edj.connectToDbs(listOf(Path.of(config.getString("MAIN_DB"))), mainDatabase = true)
+
+    config.getStringArray("ADDITIONAL_DB").forEach {
+        edj.connectToDbs(listOf(Path.of(it)), mainDatabase = false)
+    }
+
+//    var xyz = EDJPlaylist.loadFromId(173, edj)
+
+    edj.readMainData { _: Int, _: Int, _: Int, _: Int -> }
+
+//    xyz.populate(edj)
+
+//    System.exit(0)
+
+    println("${edj.trackCount()} tracks in all databases")
+
+    println("- MISSING FILES --")
+    edj.getMissingFiles().forEach {
+        println("-- $it")
+    }
+
+    edj.scanOtherDirectories(config.getStringArray("SEARCH").map{
+        Path.of(it)
+    })
+
+    var matchedFiles = edj.matchMissingFilesWithFoundFiles()
+
+    println("Found ${matchedFiles.size} matching files in new directories.")
+
+    matchedFiles.forEach {
+        if (update == true) {
+            println("Updating for ${it.key.path}")
+            try {
+                it.key.executePathUpdateSQL()
+            }
+            // TODO: Make this a app error, not an sql error ?
+            catch (e: org.sqlite.SQLiteException) {
+                println("Error updating $it.key : $e")
+            }
+        }
+        else {
+            print("Would update ${it.key.path}")
+            println("${it.key.path} --> ${it.key.newPath}")
+        }
+    }
+
+    if (update != true) {
+        println("Rerun with --update flag to perform updates")
+    }
+
+
+    println("Closing.")
+
+}
