8dcacc7c43187feff35412be674967f9d3cf9d56
Rich Green <rich@reneodlt.com>
2022-10-30 22:59:19 +0000

    Uses a proper logging config, uses apache for configuration, uses correct maven repo thing (must remove password soon)

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,10 +4,22 @@ plugins {
     kotlin("jvm") version "1.7.10"
     id("io.ktor.plugin") version "2.1.3"
     application
+    `maven-publish`
 }
 
-group = "com.reneo"
-version = "1.0-SNAPSHOT"
+fun versionString() = nf("TAG_NAME")
+
+fun nf(env: String): String {
+    val r = System.getenv(env) ?: ""
+    if (r.length > 0)
+        return r
+    else
+        return "SNAPSHOT"
+}
+
+group = "com.reneodlt"
+version = versionString()
+
 
 repositories {
     mavenLocal()
@@ -24,8 +36,8 @@ dependencies {
     implementation("org.apache.commons:commons-configuration2:2.8.0")
     implementation("commons-beanutils:commons-beanutils:1.9.4")
 
-//    implementation("com.reneodlt:edj:v0.13.0")
-    implementation("com.reneodlt:edj:SNAPSHOT")
+    implementation("com.reneodlt:edj:v0.14.0")
+//    implementation("com.reneodlt:edj:SNAPSHOT")
 
     implementation("org.apache.logging.log4j:log4j:2.19.0")
     implementation("org.slf4j:slf4j-simple:2.0.3")
@@ -54,4 +66,30 @@ tasks.withType<KotlinCompile>() {
 
 application {
     mainClass.set("MainKt")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            pom {
+
+            }
+            groupId = "com.reneodlt"
+            artifactId = "edj-cli"
+            version = versionString()
+            from(components["kotlin"])
+//            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = uri("https://maven.pkg.github.com/reneodlt/edj-cli")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
 }
