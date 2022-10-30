4601b3c23debd46fd050e7a47a1bf82a23f76818
Rich Green <rich@reneodlt.com>
2022-10-30 22:45:01 +0000

    Uses a proper logging config, uses apache for configuration, uses correct maven repo thing (must remove password soon)

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.7.10"
+    id("io.ktor.plugin") version "2.1.3"
     application
 }
 
@@ -9,6 +10,7 @@ group = "com.reneo"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    mavenLocal()
     mavenCentral()
     maven("https://maven.pkg.github.com/reneodlt/edj-lib") {
         credentials {
@@ -19,9 +21,28 @@ repositories {
 }
 
 dependencies {
+    implementation("org.apache.commons:commons-configuration2:2.8.0")
+    implementation("commons-beanutils:commons-beanutils:1.9.4")
+
+//    implementation("com.reneodlt:edj:v0.13.0")
+    implementation("com.reneodlt:edj:SNAPSHOT")
+
+    implementation("org.apache.logging.log4j:log4j:2.19.0")
+    implementation("org.slf4j:slf4j-simple:2.0.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.5")
+
     testImplementation(kotlin("test"))
-    implementation("com.reneo:edj-lib:1.0-SNAPSHOT")
 }
+
+application {
+    mainClass.set("MainKt")
+}
+
+//ktor {
+//    fatJar {
+//        archiveFileName.set("edj-cli.jar")
+//    }
+//}
 
 tasks.test {
     useJUnitPlatform()
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,13 +1,65 @@
-import edj.EDJ
+import com.reneodlt.edj.EDJ
+import kotlinx.cli.ArgParser
+import kotlinx.cli.ArgType
+import org.apache.commons.configuration2.builder.fluent.Configurations
+import java.io.File
+import java.io.FileInputStream
+import java.nio.file.Path
+import java.util.*
+
 
 fun main(args: Array<String>) {
-    println("Hello World!")
+    println("Starting...")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val parser = ArgParser("edj-cli")
+
+    val update by parser.option(ArgType.Boolean, shortName = "u", description = "Perform updates on database")
+
+    parser.parse(args)
 
     val edj = EDJ()
+    val configs = Configurations()
 
+    val config = configs.properties(File("config.properties"))
+
+    config.keys.forEach {
+        println("File exists flag: ${it} -> ${config.getString(it)}")
+    }
+
+    edj.connectToDbs(listOf(Path.of(config.getString("MAINDB"))))
+    edj.readMainData { _: Int, _: Int, _: Int, _: Int -> }
+
+    println("${edj.trackCount()} tracks in primary database")
+
+    println("- MISSING FILES --")
+    edj.getMissingFiles().forEach {
+        println("-- $it")
+    }
+
+    edj.scanOtherDirectories(config.getStringArray("SEARCH").map{
+        Path.of(it)
+    })
+
+    var matchedFiles = edj.matchMissingFilesWithFoundFiles()
+
+    println("Found ${matchedFiles.size} matching files in new directories.")
+
+    matchedFiles.forEach {
+        if (update == true) {
+            print("Updating for ${it.key.path}")
+            it.key.executePathUpdateSQL(edj.connections.first().conn)
+        }
+        else {
+            print("Would update ${it.key.path}")
+            println("${it.key.path} --> ${it.key.newPath}")
+        }
+    }
+
+    if (update != true) {
+        println("Rerun with --update flag to perform updates")
+    }
+
+
+    println("Closing.")
 
 }
